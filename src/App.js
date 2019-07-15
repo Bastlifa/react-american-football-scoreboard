048a2fb4742fd1a15c953dfb946db95ac0d29e47
Eli Sacks <bastlifa@gmail.com>
2019-07-15 09:36:15 -0400

    added BoardTeam component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import BoardTeam from "./components/BoardTeam/BoardTeam"
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -18,18 +19,11 @@ function App() {
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{lionsScore}</div>
-          </div>
+        
+          <BoardTeam isHome={true} teamName="Lions" teamScore={lionsScore} />
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{tigersScore}</div>
-          </div>
+          <BoardTeam isHome={false} teamName="Tigers" teamScore={tigersScore} />
+
         </div>
         <BottomRow />
       </section>
